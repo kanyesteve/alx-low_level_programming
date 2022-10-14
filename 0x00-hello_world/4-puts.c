0bc896e9555fe47bb727a987f5e1e9e242aa5921
kanyesteve <kanyesteve2@gmail.com>
2022-10-13 20:09:51 -0700

    learning

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-Serves as the entry for the program
+ *
+ * Return: by default zero f no error otherwise retturns non-zero value
+ */
+int main(void)
+{
+	puts("\"Programming is like bulding a multilingual puzzle");
+	return (0);
+}
