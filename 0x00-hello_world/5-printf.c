5ff6a358173f28835e1f0ac5e30aa40e949ca4c6
kanyesteve <kanyesteve2@gmail.com>
2022-10-13 20:27:42 -0700

    learning

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * maint entry point for the program.
+ *
+ * return 0, othersie non zero
+ */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
+
+
