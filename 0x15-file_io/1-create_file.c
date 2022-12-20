093c730f7c120e6505052d7d21ebeb0fcc2b1159
Busayo Adekanye <kanyesteve2@gmail.com>
2022-12-20 06:43:49 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,8 +11,8 @@
 int create_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
-	
-	filename == NULL)
+
+	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
@@ -20,14 +20,14 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	
+
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
-	
+
 	if (o == -1 || w == -1)
 		return (-1);
-	
+
 	close(o);
-	
+
 	return (1);
 }
