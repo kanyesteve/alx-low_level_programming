d224ba83566b875b7e5de2a157b9b32387d50d3e
Busayo Adekanye <kanyesteve2@gmail.com>
2022-12-20 06:46:54 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,6 +17,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* MAIN_H */
-Footer
-© 2022 GitHub, Inc.
+#endif /* MAIN_H *
